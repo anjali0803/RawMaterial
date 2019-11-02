1f8bc85310f384f55e54f43e8babfb9f03485c3b
tunedinout <narottam2911@gmail.com>
2019-11-02 15:17:59 +0530

    some optimization

--- src/components/View/ViewComponent.js
+++ src/components/View/ViewComponent.js
@@ -20,21 +20,32 @@ import OpenIncidents from '../Pages/OpenIncidents';
 
 export default class ViewComponent extends React.Component {
     render() {
+        const routes = [
+            { path: '/admin/pending-requests', component: PendingRequests },
+            { path: '/admin/all-users', component: AllUsers },
+            { path: '/inquiry/create-new-projects', component: CreateNewProjects },
+            { path: '/inquiry/projects-assigned-to-me', component: ProjectAssignedToMe },
+            { path: '/inquiry/projects-assigned-by-me', component: ProjectAssignedByMe },
+            { path: '/inquiry/all-ongoing-projects', component: AllOngoingProjects },
+            { path: '/inquiry/closed-projects', component: ClosedProjects },
+            { path: '/inquiry/archieve-projects', component: ArchieveProjects },
+            { path: '/report/generate-new-reports', component: GenerateNewProjects },
+            { path: '/report/past-reports', component: PastReports },
+            { path: '/support/create-new-incident', component: CreateNewIncident },
+            { path: '/support/open-incidents', component: OpenIncidents }
+        ];
+
+        const routeComponents = routes.map(({ path, component }) => {
+            return <Route key={path} path={path} component={component} />
+        })
+
+
+
+        
         return (
-            <div className="view-container">
+            <div className="view-container" >
                 <HashRouter>
-                    <Route path="/admin/pending-requests" component={PendingRequests} />
-                    <Route path="/admin/all-users" component={AllUsers} />
-                    <Route path="/inquiry/create-new-projects" component={CreateNewProjects} />
-                    <Route path="/inquiry/projects-assigned-to-me" component={ProjectAssignedToMe} />
-                    <Route path="/inquiry/projects-assigned-by-me" component={ProjectAssignedByMe} />
-                    <Route path="/inquiry/all-ongoing-projects" component={AllOngoingProjects} />
-                    <Route path="/inquiry/closed-projects" component={ClosedProjects} />
-                    <Route path="/inquiry/archieve-projects" component={ArchieveProjects} />
-                    <Route path="/report/generate-new-reports" component={GenerateNewProjects} />
-                    <Route path="/report/past-reports" component={PastReports} />
-                    <Route path="/support/create-new-incident" component={CreateNewIncident} />
-                    <Route path="/support/open-incidents" component={OpenIncidents} />
+                    {routeComponents}
                 </HashRouter>
             </div>
 
