68abaa27c83f64c7b7eb7a9dbab041c611b739d3
kunal15abc <kunal15abc@gmail.com>
2019-11-06 20:52:33 +0530

    table component template modified

--- src/components/Pages/ProjectAssignedByMe.js
+++ src/components/Pages/ProjectAssignedByMe.js
@@ -1,43 +1,32 @@
-import React from 'react';
-import TableComponent from '../Table/TableComponent';
-import './index.css';
-import Axios from 'axios';
-import { setDataList } from '../../actions/dataActions'
-import { connect } from 'react-redux';
-
-
-
+import React from "react";
+import TableComponent from "../Table/TableComponent";
+import "./index.css";
+import Axios from "axios";
+import { setDataList } from "../../actions/dataActions";
+import { connect } from "react-redux";
 
 class ProjectAssignedByMe extends React.Component {
-
-    render() {
-
-        return (
-            <div>
-                <TableComponent
-                    colList={this.props.colList.filter(element => {
-                        if (element.field != 'createdBy') {
-                            return element;
-                        }
-                    })}
-
-                    dataList={this.props.dataList.filter((element) => {
-                        if (element['createdBy'] == 'user1')
-                            return element;
-
-                    })} />
-            </div>
-
-
-        )
-    }
+  render() {
+    return (
+      <div>
+        <TableComponent
+          colList={this.props.colList.filter(element => {
+            if (element.field != "createdBy") {
+              return element;
+            }
+          })}
+          dataList={this.props.dataList.filter(element => {
+            if (element["createdBy"] == "user1") return element;
+          })}
+        />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    dataList: state.dataList,
-    colList: state.colList
+  dataList: state.dataList,
+  colList: state.colList
 });
 
-export default connect(
-    mapStateToProps,
-)(ProjectAssignedByMe);
+export default connect(mapStateToProps)(ProjectAssignedByMe);
