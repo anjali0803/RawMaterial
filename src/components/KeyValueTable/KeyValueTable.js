7597bf26887b930fce8d22aab2a6456440531407
tunedinout <narottam2911@gmail.com>
2019-11-08 02:08:40 +0530

    project-screen

--- src/components/KeyValueTable/KeyValueTable.js
+++ src/components/KeyValueTable/KeyValueTable.js
@@ -2,13 +2,47 @@ import React from 'react';
 import './index.css'
 import TableComponent from '../Table/TableComponent';
 import DocumentHeader from '../DocumentHeader/DocumentHeader';
+import ButtonHeader from '../ButtonHeader/ButtonHeader';
+import ProgressBar from '../Pages/ProjectScreens/ProgressBar';
+import { createHashHistory } from 'history'
+const history = createHashHistory();
 class KeyValueTable extends React.Component {
+    constructor() {
+        super();
+        this.onSave = this.onSave.bind(this);
+        this.onDelete = this.onDelete.bind(this);
+        this.state = {
+            keyValueData: [
+
+                { key: 'Queue Size', value: 12000 },
+                { key: 'Volume', value: '45 Cubic Meters' },
+                { key: 'density', value: 67 }
+            ],
+            keyValueColList: [
+                { field: 'key', header: 'Key' },
+                { field: 'value', header: 'Value' }
+
+            ]
+        }
+
+    }
+    onSave() {
+        console.log('recommendations screen save ....');
+        history.push(this.props.redirectTo);
+    }
+    onDelete() {
+        console.log('recommendations screen delete ....');
+    }
+
     render() {
+
         return (
             <div>
+                <ButtonHeader saveEnabled={this.props.saveEnabled} deleteEnabled={this.props.deleteEnabled} className="progbar-button-header" onSave={() => this.onSave()} onDelete={() => this.onDelete()} />
+                <ProgressBar steps={this.props.steps} unqURL={window.location.href.replace(window.location.origin, '')} />
                 <DocumentHeader documentId={'123456'} projectId={'121245'}
                 />
-                <TableComponent colList={this.props.colList} dataList={this.props.dataList} />
+                <TableComponent colList={this.state.keyValueColList} dataList={this.state.keyValueData} />
             </div>
         )
     }
--- src/components/Pages/ProjectScreens/OutputKeyValue.js
+++ src/components/Pages/ProjectScreens/OutputKeyValue.js
@@ -5,22 +5,155 @@ import './index.css';
 import KeyValueTable from '../../KeyValueTable/KeyValueTable';
 import InputTable from '../../InputTable/InputTable';
 import TableComponent from '../../Table/TableComponent';
+import ButtonHeader from '../../ButtonHeader/ButtonHeader';
+import ProgressBar from './ProgressBar';
 const history = createHashHistory();
-class OutputKeyValue extends React.Component {
-    onClick() {
+class Recommendations extends React.Component {
+    constructor() {
+        super();
+        this.onSave = this.onSave.bind(this);
+        this.onDelete = this.onDelete.bind(this);
+        this.state = {
+
+            tableData: [
+
+
+                {
+                    documentId: '123490',
+                    projectId: '125012',
+                    customer: 'Adante',
+                    type: 'Aplha',
+                    uploadedDate: '12-10-2017',
+                    sent: 'Yes',
+                    last: "Generated",
+                    sentOn: '24-11-2017'
+                },
+                {
+                    documentId: '123487',
+                    projectId: '125019',
+                    customer: 'Navi',
+                    type: 'Beta',
+                    uploadedDate: '11-10-2016',
+                    sent: 'No',
+                    last: "Non generated",
+                    sentOn: '24-11-2019'
+                },
+                {
+                    documentId: '123467',
+                    projectId: '125045',
+                    customer: 'Valve',
+                    type: 'Omega',
+                    uploadedDate: '12-10-2017',
+                    sent: 'Yes',
+                    last: "Generated",
+                    sentOn: '24-11-2017'
+                },
+                {
+                    documentId: '123493',
+                    projectId: '125142',
+                    customer: 'theta',
+                    type: 'Beta',
+                    uploadedDate: '12-10-2017',
+                    sent: 'Yes',
+                    last: "Generated",
+                    sentOn: '24-11-2017'
+                },
+                {
+                    documentId: '123490',
+                    projectId: '125012',
+                    customer: 'Adante',
+                    type: 'Aplha',
+                    uploadedDate: '12-10-2017',
+                    sent: 'Yes',
+                    last: "Generated",
+                    sentOn: '24-11-2017'
+                },
+                {
+                    documentId: '123490',
+                    projectId: '125012',
+                    customer: 'Adante',
+                    type: 'Aplha',
+                    uploadedDate: '12-10-2017',
+                    sent: 'Yes',
+                    last: "Generated",
+                    sentOn: '24-11-2017'
+                },
+                {
+                    documentId: '123490',
+                    projectId: '125012',
+                    customer: 'Adante',
+                    type: 'Aplha',
+                    uploadedDate: '12-10-2017',
+                    sent: 'Yes',
+                    last: "Generated",
+                    sentOn: '24-11-2017'
+                },
+                {
+                    documentId: '123490',
+                    projectId: '125012',
+                    customer: 'Adante',
+                    type: 'Aplha',
+                    uploadedDate: '12-10-2017',
+                    sent: 'Yes',
+                    last: "Generated",
+                    sentOn: '24-11-2017'
+                }
+            ],
+            tableColList: [
+                { field: 'documentId', header: 'Document Id' },
+                { field: 'projectId', header: 'Project Id' },
+                { field: 'customer', header: 'Customer' },
+                { field: 'type', header: 'Type' },
+                { field: 'uploadedDate', header: 'Uploaded Date' },
+                { field: 'sent', header: 'Sent' },
+                { field: 'last', header: 'Last' },
+                { field: 'sentOn', header: 'Sent On' }
+
+            ],
+            keyValueData: [
+
+                { key: 'Queue Size', value: 12000 },
+                { key: 'Volume', value: '45 Cubic Meters' },
+                { key: 'density', value: 67 }
+            ],
+            keyValueColList: [
+                { field: 'key', header: 'Key' },
+                { field: 'value', header: 'Value' }
+
+            ],
+
+
+
+        }
+    }
+    onDocIdClick() {
         history.push("/Inquiry/create-new-projects/output-key-value/second");
     }
+    onClick() {
+        history.push("/Inquiry/create-new-projects/output-document/second");
+    }
+    onSave() {
+        console.log('Recommendations Save..');
+        history.push("/Inquiry/create-new-projects/output-document");
+    }
+
+    onDelete() {
+        console.log('Output-key-value Delete..');
+    }
+
     render() {
 
 
 
         return (
             <div>
+                <ButtonHeader saveEnabled={this.props.saveEnabled} deleteEnabled={this.props.deleteEnabled} className="progbar-button-header" onSave={() => this.onSave()} onDelete={() => this.onDelete()} />
+                <ProgressBar steps={this.props.steps} unqURL={window.location.href.replace(window.location.origin, '')} />
                 <button onClick={this.onClick}>Click me</button>
-                <TableComponent colList={this.props.colList} dataList={this.props.dataList} />
+                <TableComponent colList={this.props.colList} dataList={this.props.dataList} onDocumentIdClick={this.onDocIdClick} />
             </div>
         )
     }
 }
 
-export default OutputKeyValue;
+export default Recommendations;
