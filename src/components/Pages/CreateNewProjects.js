0a313a7ab3dbeb4bc4179e06cf88ba83cea8a56f
tunedinout <narottam2911@gmail.com>
2019-11-08 19:46:34 +0530

    added local states to each page and css optimisations

--- src/components/Pages/CreateNewProjects.js
+++ src/components/Pages/CreateNewProjects.js
@@ -2,7 +2,6 @@ import React from 'react';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/nova-light/theme.css';
-import { Steps } from 'primereact/steps'
 import Details from './ProjectScreens/Details'
 import InputKeyValue from './ProjectScreens/InputKeyValue'
 import Recommendations from './ProjectScreens/Recommendations';
@@ -13,26 +12,13 @@ import { HashRouter, Route } from 'react-router-dom';
 import './index.css';
 import { createHashHistory } from "history";
 import ProgressBar from './ProjectScreens/ProgressBar';
-import { setProgressActiveIndex, setProjectFields } from "../../actions/dataActions";
-import { connect } from 'react-redux';
-import ButtonHeader from '../ButtonHeader/ButtonHeader';
 import KeyValueTable from '../KeyValueTable/KeyValueTable';
-import InputTable from '../InputTable/InputTable';
 const history = createHashHistory();
 
 
-console.log(window.location);
 
-const activeIndexMapping = [
-    '/Inquiry/create-new-projects/details',
-    '/Inquiry/create-new-projects/input-key-value',
-    '/Inquiry/create-new-projects/recommendations',
-    '/Inquiry/create-new-projects/acceptance',
-    '/Inquiry/create-new-projects/output-key-value',
-    '/Inquiry/create-new-projects/output-document'
-]
 
-class CreateNewProjects extends React.Component {
+export default class CreateNewProjects extends React.Component {
     constructor() {
 
         super();
@@ -161,49 +147,9 @@ class CreateNewProjects extends React.Component {
             },
 
         }
-        this.incMe = this.incMe.bind(this);
     }
 
 
-
-    onSave() {
-        //save responds to url
-        //each screen will give an object to this function 
-        //that object will be sent 
-
-
-
-
-
-
-
-
-
-        if (this.state.type == '' || this.state.customer == '' || this.state.title == '')
-            return;
-
-        console.log('Data saved');
-        console.log(`Type = ${this.state.type}`);
-        console.log(`Title = ${this.state.title}`);
-        console.log(`Customer = ${this.state.customer}`);
-        let currentActiveIndex = -1;
-        this.state.steps.forEach((element, index) => {
-
-            if (window.location.href.search(element.toLowerCase().replace(/ /g, '-')) !== -1) {
-                currentActiveIndex = index;
-            }
-        });
-        history.push(activeIndexMapping[(currentActiveIndex + 1) % 6]);
-
-
-
-    }
-    onDelete() {
-        console.log('Data deleted');
-    }
-    incMe() {
-
-    }
     recommendationsDocClick() {
         history.push('/Inquiry/create-new-projects/recommendations/second')
     }
@@ -217,8 +163,6 @@ class CreateNewProjects extends React.Component {
             },
             {
                 path: '/Inquiry/create-new-projects/input-key-value/second', component: () => <KeyValueTable
-                    colList={this.state.inputKeyValueData.keyValueColList}
-                    dataList={this.state.inputKeyValueData.keyValueData}
                     saveEnabled={true} deleteEnabled={true}
                     steps={this.state.steps}
                     redirectTo='/Inquiry/create-new-projects/recommendations'
@@ -227,22 +171,17 @@ class CreateNewProjects extends React.Component {
 
             {
                 path: '/Inquiry/create-new-projects/input-key-value', component: () => <InputKeyValue
-                    colList={this.state.inputKeyValueData.tableColList}
-                    dataList={this.state.inputKeyValueData.tableData}
                     steps={this.state.steps}
                     saveEnabled={true} deleteEnabled={true} />
             },
             {
                 path: '/Inquiry/create-new-projects/recommendations', component: () => <Recommendations
-                    colList={this.state.inputKeyValueData.tableColList}
-                    dataList={this.state.inputKeyValueData.tableData}
                     saveEnabled={true} deleteEnabled={true}
                     steps={this.state.steps} />
             },
             {
                 path: '/Inquiry/create-new-projects/recommendations/second', component: () => <KeyValueTable
-                    colList={this.state.inputKeyValueData.keyValueColList}
-                    dataList={this.state.inputKeyValueData.keyValueData}
+
                     steps={this.state.steps}
                     saveEnabled={true} deleteEnabled={true}
                     redirectTo='/Inquiry/create-new-projects/acceptance'
@@ -250,25 +189,19 @@ class CreateNewProjects extends React.Component {
             },
             {
                 path: '/Inquiry/create-new-projects/acceptance', component: () => <Acceptance
-                    colList={this.state.inputKeyValueData.tableColList}
-                    dataList={this.state.inputKeyValueData.tableData}
-                    saveEnabled={true} deleteEnabled={true}
                     steps={this.state.steps}
                     saveEnabled={true}
                     deleteEnabled={false} />
             },
             {
                 path: '/Inquiry/create-new-projects/acceptance/second', component: () => <KeyValueTable
-                    colList={this.state.inputKeyValueData.keyValueColList}
-                    dataList={this.state.inputKeyValueData.keyValueData}
                     steps={this.state.steps}
                     saveEnabled={true} deleteEnabled={true}
                     redirectTo='/Inquiry/create-new-projects/output-key-value/' />
             },
             {
                 path: '/Inquiry/create-new-projects/output-key-value', component: () => <OutputKeyValue
-                    colList={this.state.inputKeyValueData.tableColList}
-                    dataList={this.state.inputKeyValueData.tableData}
+
                     steps={this.state.steps}
                     saveEnabled={true}
                     deleteEnabled={false}
@@ -276,8 +209,7 @@ class CreateNewProjects extends React.Component {
             },
             {
                 path: '/Inquiry/create-new-projects/output-key-value/second', component: () => <KeyValueTable
-                    colList={this.state.inputKeyValueData.keyValueColList}
-                    dataList={this.state.inputKeyValueData.keyValueData}
+
                     steps={this.state.steps}
                     redirectTo='/Inquiry/create-new-projects/output-document'
                     saveEnabled={true} deleteEnabled={true} />
@@ -285,15 +217,11 @@ class CreateNewProjects extends React.Component {
 
             {
                 path: '/Inquiry/create-new-projects/output-document', component: () => <OutputDocument
-                    colList={this.state.inputKeyValueData.tableColList}
-                    dataList={this.state.inputKeyValueData.tableData}
                     steps={this.state.steps}
                     saveEnabled={true} deleteEnabled={true} />
             },
             {
                 path: '/Inquiry/create-new-projects/output-document/second', component: () => <KeyValueTable
-                    colList={this.state.inputKeyValueData.keyValueColList}
-                    dataList={this.state.inputKeyValueData.keyValueData}
                     steps={this.state.steps}
                     redirectTo='/'
                     saveEnabled={true} deleteEnabled={true}
@@ -307,33 +235,14 @@ class CreateNewProjects extends React.Component {
         })
         return (
             <div>
-                <div className="progbar">
-
-                    {/* <button onClick={() => this.incMe()} style={{ width: "40px", height: "30px", backgroundColor: 'red', float: 'right', color: 'black' }} >Inc me</button> */}
-                </div>
-
-
+                <ProgressBar steps={this.state.steps} unqURL={window.location.href.replace(window.location.origin, '')} />
                 <div className="subscreens">
                     <HashRouter>
                         {routeComponents}
                     </HashRouter>
-                    {/* <HashRouter>
-                        <Route path="/Inquiry/create-new-projects/input-key-value/screen-2" render={() => <KeyValueTable />} />
-                    </HashRouter> */}
                 </div>
             </div>
 
         )
     }
 }
-const mapStateToProps = state => ({
-    progressActiveIndex: state.progressActiveIndex
-});
-
-const mapDispatchToProps = dispatch => ({
-    setProgressActiveIndex: (progressActiveIndex) => dispatch(setProgressActiveIndex(progressActiveIndex))
-});
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(CreateNewProjects)
