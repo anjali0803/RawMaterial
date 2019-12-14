e9c05b4ebed92354e12364aa922ed432974736e8
shahid-home <shahidulkareem@gmail.com>
2019-12-14 13:18:24 +0530

    login and register screen design changes

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,3 @@
-export const backendUrl = 'http://127.0.0.1:8000';
+export const backendUrl = 'http://54.84.109.121:8000';
+//export const backendUrl = 'http://127.0.0.1:8000';
 // export const backendUrl = 'http://54.234.225.163:8000';
