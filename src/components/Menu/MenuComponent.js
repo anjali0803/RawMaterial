b8a0bade6466cba879ebfbdea6022903c8cb6ed2
dummyVariable <kumarsudhir089@gmail.com>
2020-03-06 15:17:21 +0530

    support screen done

--- src/components/Menu/MenuComponent.js
+++ src/components/Menu/MenuComponent.js
@@ -12,207 +12,110 @@ import * as router from 'react-router-dom';
 
 
 class MenuComponent extends React.Component {
-    async componentDidMount() {
-        let data;
-        data = await Axios.get(
-            `${backendUrl}/dashboard/all_project`
-        );
-        this.props.setDataList(data.data.data.Items);
+	async componentDidMount() {
+		let data;
+		data = await Axios.get(
+			`${backendUrl}/dashboard/all_project`
+		);
+		this.props.setDataList(data.data.data.Items);
 
-        const colList = [
-            { field: "ProjectID", header: "Project Id" },
-            { field: "Title", header: "Title" },
-            { field: "Client", header: "Customer" },
-            { field: "ProjectType", header: "Type" },
-            { field: "AssignedOn", header: "Assigned Date" },
-            { field: "ProjectStatus", header: "Status" },
-            { field: "AssignedTo", header: "Assigned To" },
-            { field: "CreatedBy", header: "Created By" }
-        ];
-        this.props.setColList(colList);
-    }
-    render() {
-				const toggleDropDownMenu = () => {
-					var dropdownEle = document.activeElement.className;
-					let collapsedDropDown = [];
-					$(".dashboard").removeClass("active");
-					for(let i = 0; i <= 3; i++){
-						if(dropdownEle.indexOf(i.toString()) < 0){
-							$(`.dropdown-container-${i}`).hide();
-							$(`.dropdown-btn-${i}`).removeClass("active");
-						} else {
-							$(`.dropdown-btn-${i}`).addClass("active");
-							$(`.dropdown-container-${i}`).toggle();
-						}
-					}
-				};
+		const colList = [
+			{ field: "ProjectID", header: "Project Id" },
+			{ field: "Title", header: "Title" },
+			{ field: "Client", header: "Customer" },
+			{ field: "ProjectType", header: "Type" },
+			{ field: "AssignedOn", header: "Assigned Date" },
+			{ field: "ProjectStatus", header: "Status" },
+			{ field: "AssignedTo", header: "Assigned To" },
+			{ field: "CreatedBy", header: "Created By" }
+		];
+		this.props.setColList(colList);
+	}
+	render() {
+		const toggleDropDownMenu = () => {
+			var dropdownEle = document.activeElement.className;
+			let collapsedDropDown = [];
+			$(".dashboard").removeClass("active");
+			for (let i = 0; i <= 3; i++) {
+				if (dropdownEle.indexOf(i.toString()) < 0) {
+					$(`.dropdown-container-${i}`).hide();
+					$(`.dropdown-btn-${i}`).removeClass("active");
+				} else {
+					$(`.dropdown-btn-${i}`).addClass("active");
+					$(`.dropdown-container-${i}`).toggle();
+				}
+			}
+		};
 
-        const items = [
-            {
-                name: "Dashboard",
-                icon: "pi pi-fw pi-home",
-                url: '/dashboard'
-            },
-            {
-                name: "Inquiry",
-                icon: "pi pi-fw pi-question",
-                children: [
+		const redirectToDashboard = () => {
+			var activeElement = document.activeElement.className;
+			$(".dashboard").addClass("active");
+			for (let i = 0; i <= 3; i++) {
+				$(`.dropdown-container-${i}`).hide();
+				$(`.dropdown-btn-${i}`).removeClass("active");
+			}
+			this.props.history.push('/dashboard');
+		};
 
-                    {
-                        name: "Create new projects",
-                        icon: "pi pi-fw pi-plus",
-                        url: '/inquiry/create-new-projects/new'
-                    },
-                    {
-                        name: "Projects assigned to me",
-                        icon: "pi pi-fw pi-align-left",
-                        url: "/inquiry/projects-assigned-to-me"
-                    },
-                    {
-                        name: "Projects assigned by me",
-                        icon: "pi pi-fw pi-align-right",
-                        url: "/inquiry/projects-assigned-by-me"
-                    },
-                    {
-                        name: "All ongoing projects",
-                        icon: "pi pi-fw pi-clone",
-                        url: "/inquiry/all-ongoing-projects"
-                    },
-                    {
-                        name: "Closed projects",
-                        icon: "pi pi-fw pi-copy",
-                        url: "/inquiry/closed-projects"
-                    },
-                    {
-                        name: "Archive projects",
-                        icon: "pi pi-fw pi-envelope",
-                        url: "/inquiry/archieve-projects"
-                    }
-                ]
-            },
-            {
-                name: "Report",
-                icon: "pi pi-fw pi-file-excel",
-                children: [
-                    {
-                        name: "Generate new reports",
-                        icon: "pi pi-fw pi-plus",
-                        url: "/report/generate-new-reports"
-                    },
-                    {
-                        name: "past reports",
-                        icon: "pi pi-fw pi-minus",
-                        url: "/report/past-reports"
-                    }
-                ]
-            },
-            {
-                name: "Support",
-                icon: "pi  pi-info",
-                url: "/support",
-                children: [
-                    {
-                        name: "Create new Incident",
-                        icon: "pi pi-eye",
-                        url: "/support/create-new-incident"
-                    },
-                    {
-                        name: "Open Incidents",
-                        icon: "pi pi-circle-off",
-                        url: "/support/open-incidents"
-                    }
-                ]
-            }
-        ];
-
-        this.props.userRole == "admin"
-            ? items.unshift({
-                name: "Admin",
-                icon: "pi pi-user",
-                children: [
-                    {
-                        name: "Pending Requests",
-                        icon: "pi pi-key",
-                        url: "/admin/pending-requests"
-                    },
-                    {
-                        name: "All Users",
-                        icon: "pi pi-fw pi-users",
-                        url: "/admin/all-users"
-                    }
-                ]
-            })
-            : null;
-				
-				const redirectToDashboard = () =>{
-					var activeElement = document.activeElement.className;
-					$(".dashboard").addClass("active");
-					for(let i = 0; i <= 3; i++){
-						$(`.dropdown-container-${i}`).hide();
-						$(`.dropdown-btn-${i}`).removeClass("active");
-					}
-					this.props.history.push('/dashboard');
-				};
-
-        return (
-            <div className="sideBar">
-              <div className="navMenu">
-								{
-									(this.props.userRole == "admin") && (
-										<>
-											<button class="dropdown-btn dropdown-btn-0" onClick={toggleDropDownMenu}><i class="pi pi-user"></i>Admin
+		return (
+			<div className="sideBar">
+				<div className="navMenu">
+					{
+						(this.props.userRole == "admin") && (
+							<>
+								<button class="dropdown-btn dropdown-btn-0" onClick={toggleDropDownMenu}><i class="pi pi-user"></i>Admin
 													<i class="fa fa-caret-down"></i>
-											</button>
-											<div class="dropdown-container dropdown-container-0">
-													<a href="#/support/create-new-incident"><i class="pi pi-key"></i>Pending Requests</a>
-													<a href="#/support/open-incidents"><i class="pi pi-fw pi-users"></i>All Users</a>
-											</div>
-										</>
-									)
-								}
-								<button class="dropdown-btn dashboard" onClick={redirectToDashboard}><i class="pi pi-fw pi-home"></i> Dashboard</button>
-								<button class="dropdown-btn dropdown-btn-1" onClick={toggleDropDownMenu}><i class="pi pi-fw pi-question"></i>Inquiry
+								</button>
+								<div class="dropdown-container dropdown-container-0">
+									<a href="#/support/create-new-incident"><i class="pi pi-key"></i>Pending Requests</a>
+									<a href="#/support/open-incidents"><i class="pi pi-fw pi-users"></i>All Users</a>
+								</div>
+							</>
+						)
+					}
+					<button class="dropdown-btn dashboard" onClick={redirectToDashboard}><i class="pi pi-fw pi-home"></i> Dashboard</button>
+					<button class="dropdown-btn dropdown-btn-1" onClick={toggleDropDownMenu}><i class="pi pi-fw pi-question"></i>Inquiry
                     <i class="fa fa-caret-down"></i>
-                </button>
-								<div class="dropdown-container dropdown-container-1">
-                    <a href="#/inquiry/create-new-projects/new"><i class="pi pi-fw pi-plus"></i>Create new projects</a>
-                    <a href="#/inquiry/projects-assigned-to-me"><i class="pi pi-fw pi-align-left"></i>Projects assigned to me</a>
-                    <a href="#/inquiry/projects-assigned-by-me"><i class="pi pi-fw pi-align-right"></i>Projects assigned by me</a>
-										<a href="#/inquiry/all-ongoing-projects"><i class="pi pi-fw pi-clone"></i>All ongoing projects</a>
-                    <a href="#/inquiry/closed-projects"><i class="pi pi-fw pi-copy"></i>Closed projects</a>
-                    <a href="#/inquiry/archieve-projects"><i class="pi pi-fw pi-envelope"></i>Archive projects</a>
-                </div>
-								<button class="dropdown-btn dropdown-btn-2" onClick={toggleDropDownMenu}><i class="pi pi-fw pi-file-excel"></i>Report
+					</button>
+					<div class="dropdown-container dropdown-container-1">
+						<a href="#/inquiry/create-new-projects/new"><i class="pi pi-fw pi-plus"></i>Create new projects</a>
+						<a href="#/inquiry/projects-assigned-to-me"><i class="pi pi-fw pi-align-left"></i>Projects assigned to me</a>
+						<a href="#/inquiry/projects-assigned-by-me"><i class="pi pi-fw pi-align-right"></i>Projects assigned by me</a>
+						<a href="#/inquiry/all-ongoing-projects"><i class="pi pi-fw pi-clone"></i>All ongoing projects</a>
+						<a href="#/inquiry/closed-projects"><i class="pi pi-fw pi-copy"></i>Closed projects</a>
+						<a href="#/inquiry/archieve-projects"><i class="pi pi-fw pi-envelope"></i>Archive projects</a>
+					</div>
+					<button class="dropdown-btn dropdown-btn-2" onClick={toggleDropDownMenu}><i class="pi pi-fw pi-file-excel"></i>Report
                     <i class="fa fa-caret-down"></i>
-                </button>
-								<div class="dropdown-container dropdown-container-2">
-                    <a href="#/report/generate-new-reports"><i class="pi pi-fw pi-plus"></i>Generate new reports</a>
-                    <a href="#/report/past-reports"><i class="pi pi-fw pi-minus"></i>Past reports</a>
-                </div>
-                <button class="dropdown-btn dropdown-btn-3" onClick={toggleDropDownMenu}><i class="pi  pi-info"></i>Support
+					</button>
+					<div class="dropdown-container dropdown-container-2">
+						<a href="#/report/generate-new-reports"><i class="pi pi-fw pi-plus"></i>Generate new reports</a>
+						<a href="#/report/past-reports"><i class="pi pi-fw pi-minus"></i>Past reports</a>
+					</div>
+					<button class="dropdown-btn dropdown-btn-3" onClick={toggleDropDownMenu}><i class="pi  pi-info"></i>Support
                     <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-container dropdown-container-3">
-                    <a href="#/support/create-new-incident"><i class="pi pi-eye"></i>Create new Incident</a>
-                    <a href="#/support/open-incidents"><i class="pi pi-circle-off"></i>Open Incidents</a>
-                </div>
-							</div>
-            </div>
-        );
-    }
+					</button>
+					<div class="dropdown-container dropdown-container-3">
+						<a href="#/support/create-new-incident"><i class="pi pi-eye"></i>Create new Incident</a>
+						<a href="#/support/open-incidents"><i class="pi pi-circle-off"></i>User Manual</a>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
-    userRole: state.userRole
+	userRole: state.userRole
 });
 
 const mapDispatchToProps = dispatch => ({
-    setDataList: dataList => dispatch(setDataList(dataList)),
-    setColList: colList => dispatch(setColList(colList)),
+	setDataList: dataList => dispatch(setDataList(dataList)),
+	setColList: colList => dispatch(setColList(colList)),
 });
 
 
 export default router.withRouter(connect(
-    mapStateToProps,
-    mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(MenuComponent));
--- src/components/Pages/OpenIncidents.js
+++ src/components/Pages/OpenIncidents.js
@@ -1,14 +1,121 @@
 import React from 'react';
 import './index.css';
 
-
-
-
 export default class OpenIncidents extends React.Component {
-    render() {
-        return (
-            <div>Open incidents</div>
+	constructor(props) {
+		super(props);
+		this.videoTutorialList = [
+			{
+				title: 'video 1',
+				url: ''
+			},
+			{
+				title: 'video 2',
+				url: ''
+			}
+		];
 
-        )
-    }
+		this.documentationList = [
+			{
+				text: 'Create New Project',
+				url: ''
+			},
+			{
+				text: 'Admin Controls',
+				url: ''
+			},
+			{
+				text: 'Update Project Details',
+				url: ''
+			},
+			{
+				text: 'Update Calculation Sheet',
+				url: ''
+			},
+			{
+				text: 'Update ITP and its versions',
+				url: ''
+			},
+			{
+				text: 'Update Comment Sheet and its versions',
+				url: ''
+			},
+			{
+				text: 'Download reports',
+				url: ''
+			},
+			{
+				text: 'Raise Incident',
+				url: ''
+			}
+		];
+
+		this.toggleDiv = this.toggleDiv.bind(this);
+	}
+
+	toggleDiv(e) {
+		const currentSelection = document.activeElement.className;
+		const currentSelectedVideoIndex = Number(currentSelection[currentSelection.length -1])
+		for(let i=0; i <= this.videoTutorialList.length; i++){
+			if(i === currentSelectedVideoIndex){
+				$(`.collapse-${currentSelectedVideoIndex}`).toggle();
+			} else {
+				$(`.collapse-${i}`).hide();
+			}
+
+		}
+	}
+	render() {
+		
+
+		return (
+			<div style={{ backgroundColor: 'white', paddingRight: '100px', paddingLeft: '100px', paddingTop: '50px', paddingBottom: '200px' }}>
+				<div className="row justify-content-center">
+					<div className="col-6 justify-content-center" style={{ borderRight: '1px solid #ced4da', height: '100%' }}>
+						<h1>
+							Documentations
+						</h1>
+						<hr />
+						<hr />
+						<ul class="list-group">
+							{
+								this.documentationList.map(listElement => {
+									return (
+										<a style={{ color: 'black', fontSize: '16px' }} href={listElement.url}><li class="list-group-item">{listElement.text}</li></a>
+									)
+								})
+							}
+						</ul>
+					</div>
+					<div className="col-6 justify-content-center">
+						<h1>
+							Video Tutorials
+						</h1>
+						<hr />
+						<hr />
+						<div className="justify-content-center">
+							{
+								this.videoTutorialList.map((videoElement, key) => {
+									return (
+										<>
+											<div class="card">
+												<div class="card-header">
+													<h5 class="mb-0">
+														<button class={`btn btn-link ${key}`} onClick={this.toggleDiv}>{videoElement.title}</button>
+													</h5>
+												</div>
+												<div className={`collapse-${key}`} style={{display: 'none'}}>
+												<iframe className="videoPlayerBox" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+												</div>
+											</div>
+										</>
+									)
+								})
+							}
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
