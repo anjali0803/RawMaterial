6f392514175d9011eda24b573c69fdd0ae002c89
dummyVariable <kumarsudhir089@gmail.com>
2020-03-16 16:41:49 +0530

    changes for mdal

--- src/components/Pages/FormatDocForm.js
+++ src/components/Pages/FormatDocForm.js
@@ -96,11 +96,8 @@ export class FormatDocForm extends React.Component {
               <Input onChange={this.saveCSFormat} placeholder="Please enter issue CS format" required/>
             </div>
             <button className="saveButton" onClick={this.onSave} type="submit">Update Format</button>
-            <Dialog header="Godfather I" visible={this.state.visible} style={{width: '50vw'}} modal={true} onHide={() => this.setState({visible: false})}>
-              The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding.
-              His beloved son Michael has just come home from the war, but does not intend to become part of his father's business.
-              Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family,
-              kind and benevolent to those who give respect, but given to ruthless violence whenever anything stands against the good of the family.
+            <Dialog header="Please Wait..." visible={this.state.visible} style={{ width: '50vw' }} modal={true}>
+              Your response is getting submitted!!
             </Dialog>
           </div>
         </div>
