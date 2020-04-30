b28581a2302ef33e7597b352508333b0d534309d
root <root@app-server.hfeo0tzpv5putkobporobu4emd.gx.internal.cloudapp.net>
2020-04-30 21:29:28 +0000

    docker file added

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
 // export const backendUrl = 'http://54.84.109.121:8000';
-export const backendUrl = 'https://cipbackend.azurewebsites.net'
+export const backendUrl = 'https://cipbackend-uat.azurewebsites.net'
 // export const backendUrl = 'http://localhost:8000'
 export const authenticationUrl = 'https://cipauth.azurewebsites.net'
 // export const backendUrl = 'http://54.234.225.163:8000';
