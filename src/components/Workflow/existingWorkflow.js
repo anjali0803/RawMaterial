c934bee5d24b703de6e5d283e3f0aaa86e46aa64
Akshat Anand <plasmatiger@Akshats-MacBook-Pro.local>
2020-06-03 02:54:24 +0530

    Existing workflow layout done. Data need to be integrated

--- src/components/Workflow/existingWorkflow.js
+++ src/components/Workflow/existingWorkflow.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import { Container, Row, Col, Card, CardTitle, CardBody, CardFooter, Progress } from 'reactstrap'
+import React, { Component } from 'react'
+import { Container, Row, Col, Card, CardTitle, CardSubtitle, CardText, CardBody, CardFooter, Progress, Button } from 'reactstrap'
+// import './workflow.scss'
 
-export default class ExistingWorkflow extends React.Component{
-    render(){
-        return <Container fluid = {true}>
-            Existing Workflow
+// import {Card} from 'primereact/card';
+// import {Button} from 'primereact/button';
+
+export default class ExistingWorkflow extends React.Component {
+    render() {
+        return <Container fluid={true}>
+            <Row>
+                <WorkflowCard title={"Title"} subtitle={"Subtitle"} text={"Text"}/>
+                <WorkflowCard title={"Title2"} subtitle={"Subtitle2"} text={"Text2"}/>
+            </Row>
+
         </Container>
     }
 }
+
+
+const WorkflowCard = (props) => {
+    return (
+        <div>
+            <Container style={{padding:'30px'}}>
+            <Card body>
+                <CardBody>
+                    <CardTitle>{props.title}</CardTitle>
+                    <CardSubtitle>{props.subtitle}</CardSubtitle>
+                    {/* </CardBody> */}
+                    {/* <img width="100%" src="/assets/318x180.svg" alt="Card image cap" /> */}
+                    {/* <CardBody> */}
+                    <CardText>{props.text}</CardText>
+                    {/* <CardLink href="#">Card Link</CardLink>
+            <CardLink href="#">Another Link</CardLink> */}
+                    <Button style={{margin:'10px'}}>Save</Button>
+                    <Button style={{margin:'10px'}}>Cancel</Button>
+                </CardBody>
+            </Card>
+            </Container>
+        </div>
+    );
+};
--- src/components/Workflow/newWorkflow.js
+++ src/components/Workflow/newWorkflow.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Container, Row, Col, Card, CardTitle, CardBody, CardFooter, Progress } from 'reactstrap'
 
 export default class NewWorkflow extends React.Component{
